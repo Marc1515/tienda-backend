a0677ef61f9938f77bedcc041a714ddba31479b1
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-23 12:46:12 +0200

    En la entidad solo he dado unos espacios para una mejor legibilidad del código.

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -4,12 +4,16 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 export class User {
   @Prop({ unique: true, required: true })
   email: string;
+
   @Prop({ required: true })
   name: string;
+
   @Prop({ minlength: 6, required: true })
   password: string;
+
   @Prop({ default: true })
   isActive: boolean;
+
   @Prop({ type: [String], default: ['user'] })
   roles: string[];
 }
