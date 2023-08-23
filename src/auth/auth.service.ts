5d31b2bbcf27626a08f1d7b4ead3ed6a1d661da6
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-23 14:04:29 +0200

    Importamos la librería bcryptjs en el servicio auth y en la creación del usuario sacamos con la desestructuración el password y el resto de la data del usuario con el operador rest. Luego en una nueva variable guardamos la contraseña encriptada y el resto de data del usuario. Finalmente, devolvemos el usuario sin la contraseña al backend.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,8 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import * as bcryptjs from 'bcryptjs';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
@@ -16,13 +18,21 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+      /* return await newUser.save(); */
+
+      const { password: _, ...user } = newUser.toJSON();
+
+      return user;
 
       // 1 - Encriptar la contraseña.
       // 2 - Guardar usuario.
       // 3 - Generar un JWT.
-
-      return await newUser.save();
     } catch (error) {
       if (error.code === 11000) {
         throw new BadRequestException(`${createUserDto.email} already exist!`);
