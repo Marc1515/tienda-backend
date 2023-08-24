9c6e641547ddcf3847fe4cabc9a28a656aade8e5
Marc España Quinquilla <meq.1515@gmail.com>
2023-08-24 14:17:59 +0200

    Creo un nuevo DTO para la parte del registro, ya que el objetivo es diferente al de crear usuario.

---
+++ src/auth/dto/register-user.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { IsEmail, MinLength, IsString } from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(6)
+  password: string;
+}
